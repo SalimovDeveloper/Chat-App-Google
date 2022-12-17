f822a37579337070183c5c9b12b55427035e9d67
salimovdeveloper <salimovdeveloper@gmail.com>
2022-12-16 21:24:16 -0600

    commit 2

---
+++ app/src/main/java/uz/salimovdeveloper/chatappgoogle/fragment/HomeFragment.kt
@@ -0,0 +1,21 @@
+package uz.salimovdeveloper.chatappgoogle.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.salimovdeveloper.chatappgoogle.R
+import uz.salimovdeveloper.chatappgoogle.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/uz/salimovdeveloper/chatappgoogle/fragment/SignUpFragment.kt
@@ -0,0 +1,21 @@
+package uz.salimovdeveloper.chatappgoogle.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.salimovdeveloper.chatappgoogle.R
+import uz.salimovdeveloper.chatappgoogle.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSignUpBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+}
