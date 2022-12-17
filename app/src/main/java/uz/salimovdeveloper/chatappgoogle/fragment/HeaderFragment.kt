b48d13b281eacffd742b6a37a9b12a016c20ae44
salimovdeveloper <salimovdeveloper@gmail.com>
2022-12-16 21:30:36 -0600

    commit 2

---
+++ app/src/main/java/uz/salimovdeveloper/chatappgoogle/fragment/HeaderFragment.kt
@@ -0,0 +1,21 @@
+package uz.salimovdeveloper.chatappgoogle.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import uz.salimovdeveloper.chatappgoogle.R
+import uz.salimovdeveloper.chatappgoogle.databinding.FragmentHeaderBinding
+
+class HeaderFragment : Fragment() {
+    private lateinit var binding: FragmentHeaderBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHeaderBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+}
